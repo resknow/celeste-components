4052795ae6d3b397b083cacc885c185838a70462
Chris Galbraith <chris@resknow.co.uk>
2024-03-22 08:59:27 +0000

    Fix Post Card block

--- components/PostCard/component.php
+++ components/PostCard/component.php
@@ -2,15 +2,18 @@
 
 add_filter( 'twilight.component.PostCard', function( $context ) {
 
-    props( 'PostCard', $context, [
-        'id' => [ 'type' => 'number' ]
-    ] );
+    if ( version_compare( $context['utils']['theme_version'], '1.1.0', '<' ) ) {
+        props( 'PostCard', $context, [
+            'id' => [ 'type' => 'number' ]
+        ] );
+    }
 
     if ( get_post_status( $context['id'] ) === false ) {
         celeste_warn( sprintf( '<code>`PostCard`</code> Post with ID <code>`%s`</code> does not exist.', $context['id'] ) );
     }
 
-    $context['post'] = get_full_post( $context['id'] );
+    $context['post'] = get_post( $context['id'] );
+    $context['post']->featured_image = get_post_thumbnail_id( $context['id'] );
 
     return $context;
 } );
