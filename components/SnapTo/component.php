2c74b96e505e63a671c5bd7b05847787ada44ddd
Chris Galbraith <chris@resknow.co.uk>
2024-03-22 08:25:32 +0000

    Make SnapTo compatible with 1.0.0

--- components/SnapTo/component.php
+++ components/SnapTo/component.php
@@ -2,12 +2,14 @@
 
 add_filter( 'twilight.component.SnapTo', function( $context ) {
 
-    /**
-     * Define Prop Types and split props from the rest of the context
-     */
-    [ $props, $rest ] = props( 'SnapTo', $context, include __DIR__ . '/prop-types.php' );
-    $context = array_merge( $context, $props );
-    $context['attributes'] = attributes($rest);
+    if ( version_compare( $context['utils']['theme_version'], '1.1.0', '<' ) ) {
+        /**
+         * Define Prop Types and split props from the rest of the context
+         */
+        [ $props, $rest ] = props( 'SnapTo', $context, include __DIR__ . '/prop-types.php' );
+        $context = array_merge( $context, $props );
+        $context['attributes'] = attributes($rest);
+    }
 
     return $context;
 } );
