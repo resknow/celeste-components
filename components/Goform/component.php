00f2cade4890eef3124da2be6b6b33df31e44a35
Chris Galbraith <chris@resknow.co.uk>
2024-05-01 16:59:25 +0100

    Remove Goform inputs in favour of Input & Select components

--- components/Goform/component.php
+++ components/Goform/component.php
@@ -6,3 +6,11 @@ add_filter( 'twilight.component.Goform.present', function( $context ) {
     Assets::add( 'script', 'goform', 'https://goform.app/wc.js' );
     return $context;
 } );
+
+add_filter( 'twilight.component.Goform', function( $context ) {
+    [ $props, $rest ] = props( 'Goform', $context, include __DIR__ . '/prop-types.php' );
+    $context = array_merge( $context, $props );
+    $context['attributes'] = attributes($rest);
+
+    return $context;
+} );
